5d9b00ce6c6ce868b276291f34b57157d5dbb407
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2024-01-31 22:56:21 +0530

    adding the project with routing and login form

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup | any;
+  constructor(private fb: FormBuilder){}
+  ngOnInit(){
+
+  this.loginForm=this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+}
+
+
+
+}
